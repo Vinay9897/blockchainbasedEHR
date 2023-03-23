b748aa54a8ca975a7de51cf4e6ef2fad0d0a8662
Vinay Yadav <vinayyadav91190@gmail.com>
2023-03-23 11:58:52 +0530

    write Code for display Consultation.js file

---
+++ clients/src/Components/common/displayConsultation.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Card } from 'antd';
+
+class displayConsultation extends Component {
+    render() {
+
+        let { doctor_id, consultation_advice, medicine, time_period } = this.props.props;
+
+
+        return (
+            <div>
+                <Card bordered='true' color='blue'>
+                    <b>Doctor ID</b> <br></br>
+                    <p >{doctor_id}</p>
+                    <b>Consultation</b> <br></br>
+                    <p >{consultation_advice}</p>
+                    <b>medicine</b> <br></br>
+                    <p >{medicine}</p>
+                    <b>Time period</b> <br></br>
+                    <p >{time_period}</p>
+                </Card>
+            </div>
+        );
+    }
+}
+
+
+export default displayConsultation;
---
+++ clients/src/Components/common/display_files.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Card, Icon } from 'antd';
+
+import '../css/display_file.css'
+class display_file extends Component {
+
+    render() {
+
+        let fhash = this.props.props;
+        let that = this.props.that;
+        return (
+            <div>
+                <Card title={fhash[0]} bordered={true}>
+                    <button className='button-81' type="primary" onClick={that.showFile.bind(that, fhash, true)}><Icon type="file" />Show File</button>
+                </Card>
+            </div>
+        );
+    }
+}
+
+
+export default display_file;
