eb728e650e3354fe5eaed5c032d192f078f0d6e5
Vinay Yadav <vinayyadav91190@gmail.com>
2023-03-24 19:40:55 +0530

    NavbarComp.js  design

---
+++ clients/src/Components/NavbarComp.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Form, Button, Navbar, Nav } from 'react-bootstrap';
+
+
+class NavbarComp extends Component {
+
+    render() {
+        let isLogged = this.props.isLogged ? true : false;
+
+        return (
+
+            <div bg="dark">
+
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">EHR using blockchain</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        { }
+                    </Nav>
+                    <Form inline>
+                        { }
+                        {isLogged ?
+                            <Button variant="outline-light" onClick={() => this.props.onlogout()}>Logout</Button> : <div></div>}
+                    </Form>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+
+export default NavbarComp;
